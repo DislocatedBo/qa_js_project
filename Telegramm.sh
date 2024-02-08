b367f7189463746f4427278013673ecf902d34e6
DislocatedBo <62541329+DislocatedBo@users.noreply.github.com>
2024-02-08 23:31:13 +0700

    Add files via upload

---
+++ Telegramm.sh
@@ -0,0 +1,7 @@
+java \
+"-DprojectName=E2E Test Merchant Cabinet" \
+"-Denv=e2e" \
+"-DreportLink=https://elementpay.testops.cloud/project/2/launches" \
+"-Dcomm=E2E UI Test Merchant Cabinet" \
+"-Dconfig.file=./allure-notifications.json" \
+-jar allure-notifications-3.1.2.jar
